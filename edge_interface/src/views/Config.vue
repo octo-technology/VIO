f7ac71750d8e952acca00fd48d55ca090903a412
LouisonR-octo <louison.roger@octo.com>
2022-12-23 16:09:44 +0100

    Fix github action npm run lint

--- edge_interface/src/views/Config.vue
+++ edge_interface/src/views/Config.vue
@@ -2,71 +2,97 @@
   <div class="container">
     <h3>VIO Configuration</h3>
     <v-card
-        v-for="(item_config, item_category) in configs" :key="item_category" elevation="5"
-        class="config-card"
+      v-for="(item_config, item_category) in configs"
+      :key="item_category"
+      elevation="5"
+      class="config-card"
     >
       <div class="item-category">
         <input
-            id="category"
-            class="input-radio"
-            type="radio"
-            :value="item_category"
-            v-model="selected_item_category"/>
+          id="category"
+          class="input-radio"
+          type="radio"
+          :value="item_category"
+          v-model="selected_item_category"
+        />
         <label for="category">{{ item_category }}</label>
       </div>
       <v-simple-table>
         <template>
           <thead>
-          <tr>
-            <td v-for="(camera_config, camera_id) in item_config['cameras']" :key="camera_id">
-              <span class="title-1">{{ camera_id }}</span>
-              <div class="title-2">Settings</div>
-              <div>
-                <div> Type: {{ camera_config["type"] }} </div>
-                <div> Position: {{ camera_config["position"] }} </div>
-                <div> Exposition: {{ camera_config["exposition"] }} </div>
+            <tr>
+              <td
+                v-for="(camera_config, camera_id) in item_config['cameras']"
+                :key="camera_id"
+              >
+                <span class="title-1">{{ camera_id }}</span>
+                <div class="title-2">Settings</div>
+                <div>
+                  <div>Type: {{ camera_config["type"] }}</div>
+                  <div>Position: {{ camera_config["position"] }}</div>
+                  <div>Exposition: {{ camera_config["exposition"] }}</div>
+                </div>
 
-              </div>
+                <div class="title-2">Models</div>
+                <div
+                  v-for="(model, model_id) in camera_config['models_graph']"
+                  :key="model_id"
+                >
+                  <span class="title-3">{{ model_id }}</span>
+                  <div>Model name: {{ model["metadata"] }}</div>
+                  <div>
+                    Model type:
+                    {{ inventory["models"][model["metadata"]]["category"] }}
+                  </div>
+                  <div>
+                    Model version:
+                    {{ inventory["models"][model["metadata"]]["version"] }}
+                  </div>
+                  <div>
+                    Class names:
+                    {{ inventory["models"][model["metadata"]]["class_names"] }}
+                  </div>
+                  <div>Depends on: {{ model["depends_on"] }}</div>
+                </div>
 
-              <div class="title-2">Models</div>
-              <div v-for="(model, model_id) in camera_config['models_graph']" :key="model_id">
-                <span class="title-3">{{ model_id }}</span>
-                <div> Model name: {{ model["metadata"] }} </div>
-                <div> Model type: {{inventory["models"][model["metadata"]]["category"]}} </div>
-                <div> Model version: {{inventory["models"][model["metadata"]]["version"]}} </div>
-                <div> Class names: {{inventory["models"][model["metadata"]]["class_names"]}} </div>
-                <div> Depends on: {{ model['depends_on'] }}</div>
-              </div>
-
-              <div class="title-2">Camera Business Rule</div>
-              <div class="title-3"> Name </div>
-              <div> {{ camera_config['camera_rule']['name'] }}</div>
-              <span class="title-3"> Parameters </span>
-             <div v-for="(camera_rule, camera_rule_id) in camera_config['camera_rule']['parameters']" :key="camera_rule_id" >
+                <div class="title-2">Camera Business Rule</div>
+                <div class="title-3">Name</div>
+                <div>{{ camera_config["camera_rule"]["name"] }}</div>
+                <span class="title-3"> Parameters </span>
+                <div
+                  v-for="(camera_rule, camera_rule_id) in camera_config[
+                    'camera_rule'
+                  ]['parameters']"
+                  :key="camera_rule_id"
+                >
                   <span>{{ camera_rule_id }}: {{ camera_rule }}</span>
-              </div>
-            </td>
+                </div>
+              </td>
 
-            <td >
-            <div class="title-1">Item Business Rule</div>
-              <div class="title-3"> Name </div>
-              <div> {{ item_config['item_rule']['name'] }}</div>
-              <span class="title-3"> Parameters </span>
-             <div v-for="(item_rule, item_rule_id) in item_config['item_rule']['parameters']" :key="item_rule_id" >
+              <td>
+                <div class="title-1">Item Business Rule</div>
+                <div class="title-3">Name</div>
+                <div>{{ item_config["item_rule"]["name"] }}</div>
+                <span class="title-3"> Parameters </span>
+                <div
+                  v-for="(item_rule, item_rule_id) in item_config['item_rule'][
+                    'parameters'
+                  ]"
+                  :key="item_rule_id"
+                >
                   <span>{{ item_rule_id }}: {{ item_rule }}</span>
-              </div>
-            </td>
-
-          </tr>
+                </div>
+              </td>
+            </tr>
           </thead>
         </template>
       </v-simple-table>
     </v-card>
 
     <v-btn
-        v-on:click="changeConfiguration(selected_item_category)"
-        class="validation-button"
-    >Valider
+      v-on:click="changeConfiguration(selected_item_category)"
+      class="validation-button"
+      >Valider
     </v-btn>
   </div>
 </template>
@@ -86,9 +112,12 @@ export default {
   },
 
   created() {
-    ConfigService.get_configs().then(response => (this.configs = response.data));
-    ConfigService.get_inventory().then(response => (this.inventory = response.data));
-
+    ConfigService.get_configs().then(
+      response => (this.configs = response.data)
+    );
+    ConfigService.get_inventory().then(
+      response => (this.inventory = response.data)
+    );
   },
   methods: {
     changeConfiguration(item_category) {
@@ -97,7 +126,6 @@ export default {
   }
 };
 </script>
-
 
 <style lang="scss" scoped>
 h3 {
