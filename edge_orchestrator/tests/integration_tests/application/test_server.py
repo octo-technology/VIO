b365e42e59ca000846214581edcf4c42e11f1898
gireg.roussel <giregroussel@free.fr>
2023-02-24 15:45:12 +0100

    fix upload route test

--- edge_orchestrator/tests/integration_tests/application/test_server.py
+++ edge_orchestrator/tests/integration_tests/application/test_server.py
@@ -1,14 +1,16 @@
+import os
 from fastapi.testclient import TestClient
 from tests.conftest import TEST_DATA_FOLDER_PATH
 
-from edge_orchestrator.application.server import server
+os.environ['API_CONFIG'] = 'default'
 
-client = TestClient(server())
+from edge_orchestrator.application.server import server
 
 
 class TestServer:
-    def test_upload_route__should_return_expected_logs_when_received_paylod_with_binary_image(self, test_mongo_db_uri, caplog):
+    def test_upload_route__should_return_expected_logs_when_received_paylod_with_binary_image(self, caplog):
         # Given
+        client = TestClient(server())
         test_file = 'camera_id1.jpg'
         test_file_path = TEST_DATA_FOLDER_PATH / 'item_2' / test_file
         expected_logs = ["Starting Capture",
