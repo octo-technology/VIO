0894082e7f3ad2b9897f8ef6a7623797ae8577e4
gireg.roussel <giregroussel@free.fr>
2023-02-27 10:33:43 +0100

    add unit test on save item on gcp

---
+++ edge_orchestrator/tests/unit_tests/infrastructure/binary_storage/test_gcp_binary_storage.py
@@ -0,0 +1,32 @@
+from unittest.mock import patch, Mock
+from google.cloud import storage
+from datetime import datetime
+
+from edge_orchestrator.domain.models.item import Item
+from edge_orchestrator.infrastructure.binary_storage.gcp_binary_storage import GCPBinaryStorage
+
+from tests.conftest import TEST_DATA_FOLDER_PATH
+
+class TestGCPBinaryStorage:
+    @patch('edge_orchestrator.infrastructure.binary_storage.gcp_binary_storage.storage')
+    def test_save_item_binaries_should_write_image_in_gcp(self, mock_storage):
+        # Given
+        test_camera_id = '1'
+        test_file = 'camera_id1.jpg'
+        test_file_path = TEST_DATA_FOLDER_PATH / 'item_2' / test_file
+        item = Item.from_nothing()
+        with open(test_file_path, "rb") as f:
+            item.binaries = {camera_id: f}
+
+        mock_gcs_client = mock_storage.Client.return_value
+        mock_bucket = Mock()
+        mock_gcs_client.get_bucket.return_value = mock_bucket
+        gcs = GCPBinaryStorage('fake_path')
+
+        # When
+        gcs.save_item_binaries(item)
+
+        # Then
+        mock_storage.Client.assert_called_once()
+        mock_bucket.blob.assert_called_once_with(f"{datetime.now().strftime("%d-%m-%Y")}_{item.id}/{camera_id}.jpg")
+        mock_bucket.blob.return_value.upload_from_string.assert_called_once_with(f, content_type="image/jpg")
