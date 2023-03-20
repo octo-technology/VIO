67278e719790c7762f3eb6edab4973fd0923c64d
gireg.roussel <giregroussel@free.fr>
2023-03-20 15:38:54 +0100

    Merge trigger/image with simple trigger

--- edge_orchestrator/edge_orchestrator/application/trigger_routes.py
+++ edge_orchestrator/edge_orchestrator/application/trigger_routes.py
@@ -11,7 +11,7 @@ uploader = Uploader()
 
 
 @trigger_router.put('/trigger')
-async def trigger_job(background_tasks: BackgroundTasks = None):
+async def trigger_job(image: UploadFile = None, background_tasks: BackgroundTasks = None):
     item = Item.from_nothing()
     if supervisor.station_config.active_config is None:
         return JSONResponse(
@@ -20,26 +20,11 @@ async def trigger_job(background_tasks: BackgroundTasks = None):
                                 "Set the active station configuration before triggering the inspection."},
         )
     else:
+        if image:
+            contents = image.file.read()
+            item.binaries = {'0': contents}
         background_tasks.add_task(supervisor.inspect, item)
         return {'item_id': item.id}
-
-
-@trigger_router.post('/trigger/image')
-async def trigger_image_job(image: UploadFile = File(...), background_tasks:
-                            BackgroundTasks = None):
-    item = Item.from_nothing()
-    if supervisor.station_config.active_config is None:
-        return JSONResponse(
-            status_code=403,
-            content={"message": "No active configuration selected! "
-                                "Set the active station configuration before triggering the inspection."},
-        )
-    else:
-        contents = image.file.read()
-        item.binaries = {'0': contents}
-        background_tasks.add_task(supervisor.inspect, item)
-        return {'item_id': item.id}
-
 
 @trigger_router.post('/upload')
 async def upload_job(image: UploadFile = File(...), background_tasks:
