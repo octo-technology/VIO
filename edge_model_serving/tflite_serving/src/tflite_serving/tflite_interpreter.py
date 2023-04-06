b40e94cf82924149fc95ea2804435bdf4b628d6e
Baptiste O'Jeanson <bojeanson@octo.com>
2023-04-06 18:30:28 +0200

    fix: put back models inside Docker image and conf

--- edge_model_serving/tflite_serving/src/tflite_serving/tflite_interpreter.py
+++ edge_model_serving/tflite_serving/src/tflite_serving/tflite_interpreter.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from typing import Dict
 
@@ -12,7 +13,8 @@ def create_interpreter(model_path: str) -> Interpreter:
 
 def create_model_interpreters() -> Dict[str, Interpreter]:
     model_interpreters = {}
-    tflite_model_path = Path.cwd().parent / 'models' / 'tflite'
+    models_path = Path(os.getenv("MODELS_PATH")) if os.getenv("MODELS_PATH") else Path.cwd().parent
+    tflite_model_path = models_path / 'tflite'
     for model_path in tflite_model_path.glob('**/*.tflite'):
         model_interpreters[model_path.parent.name] = create_interpreter(model_path.as_posix())
     return model_interpreters
