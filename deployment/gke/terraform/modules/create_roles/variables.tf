ceadc3793c083ca46c089999a3e231e60f1bd7b1
gireg.roussel <giregroussel@free.fr>
2023-04-21 11:52:26 +0200

    Add Service Account creation to create everything from it

---
+++ deployment/gke/terraform/modules/create_roles/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  type = string
+  description = "project id"
+}
+
+variable "service_account_roles" {
+  type        = list(string)
+  description = "List of dataset names for which the Service Account will be data viewer"
+  default     = []
+}
+
