d21b09d932400aef880b5f1f77c6318a4289af70
gireg.roussel <giregroussel@free.fr>
2023-05-10 12:41:09 +0200

    Add new config for VIO api

--- edge_orchestrator/edge_orchestrator/environment/edge_with_gcp_container_storage.py
+++ edge_orchestrator/edge_orchestrator/environment/edge_with_gcp_container_storage.py
@@ -3,11 +3,12 @@ from edge_orchestrator.infrastructure.binary_storage.gcp_binary_storage import G
 from edge_orchestrator.infrastructure.metadata_storage.gcp_metadata_storage import GCPMetadataStorage
 from edge_orchestrator.infrastructure.inventory.json_inventory import JsonInventory
 from edge_orchestrator.infrastructure.station_config.json_station_config import JsonStationConfig
-from edge_orchestrator.infrastructure.model_forward.fake_model_forward import FakeModelForward
+from edge_orchestrator.infrastructure.model_forward.torch_serving_detection_and_classification_wrapper import TorchServingDetectionClassificationWrapper
 from edge_orchestrator.infrastructure.telemetry_sink.fake_telemetry_sink import FakeTelemetrySink
 
 
-class UploadWithGCPBucket(Config):
+class EdgeWithGCPContainerStorage(Config):
+    SERVING_MODEL_URL = os.environ.get('SERVING_MODEL_URL', 'http://edge_model_serving:8080')
 
     def __init__(self):
         self.metadata_storage = GCPMetadataStorage()
@@ -15,5 +16,6 @@ class UploadWithGCPBucket(Config):
         self.inventory = JsonInventory(self.ROOT_PATH / 'config' / 'inventory.json')
         self.station_config = JsonStationConfig(self.ROOT_PATH / 'config' / 'station_configs',
                                                 self.inventory, self.ROOT_PATH / 'data')
-        self.model_forward = FakeModelForward()                                                                        self.ROOT_PATH / 'data' / 'yolo_class.txt', 640)
+        self.model_forward = TorchServingDetectionClassificationWrapper(self.SERVING_MODEL_URL,
+                                                                        self.ROOT_PATH / 'data' / 'yolo_class.txt', 640)
         self.telemetry_sink = FakeTelemetrySink()
