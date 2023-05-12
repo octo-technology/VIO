103d5708b7c09568ff7f75b21a461657be4a2d40
gireg.roussel <giregroussel@free.fr>
2023-05-12 16:14:23 +0200

    make distinct ingress and add global static ip

--- deployment/gke/terraform/modules/create_infra_ressources/static_ip.tf
+++ deployment/gke/terraform/modules/create_infra_ressources/static_ip.tf
@@ -1,11 +1,11 @@
-resource "google_compute_address" "edge-model-serving" {
+resource "google_compute_global_address" "edge-model-serving" {
   name = "tf-vio-edge-model-serving"
 }
 
-resource "google_compute_address" "edge-orchestrator" {
+resource "google_compute_global_address" "edge-orchestrator" {
   name = "tf-vio-edge-orchestrator"
 }
 
-resource "google_compute_address" "edge-interface" {
+resource "google_compute_global_address" "edge-interface" {
   name = "tf-vio-edge-interface"
 }
