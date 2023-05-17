af6c7aba896e5ab415aedddb4103ed07ad779c02
gireg.roussel <giregroussel@free.fr>
2023-05-17 12:22:02 +0200

    move files and add readme

--- deployment/gke/terraform/modules/create_infra_ressources/artifact_registery.tf
+++
@@ -1,6 +0,0 @@
-resource "google_artifact_registry_repository" "main" {
-  location      = var.region
-  repository_id = local.repo_id
-  description   = "Docker repository for ${var.project_name}"
-  format        = "DOCKER"
-}
