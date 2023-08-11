1fa192e8420c86a03cfd6c83b67131aad00f1678
Baptiste O'Jeanson <bojeanson@octo.com>
2023-08-11 18:09:24 +0200

    refactor: add main function to launch server

--- edge_orchestrator/edge_orchestrator/__main__.py
+++ edge_orchestrator/edge_orchestrator/__main__.py
@@ -2,6 +2,11 @@ import uvicorn
 
 from edge_orchestrator.application.server import server
 
-if __name__ == "__main__":
+
+def main():
     orchestrator_app = server()
     uvicorn.run(orchestrator_app, host="0.0.0.0", port=8000, log_level="info")
+
+
+if __name__ == "__main__":
+    main()
