4541a80705b4daff80b6c6c764bf66cc9eff8c7b
Thibaut <thibaut.leibel@octo.com>
2024-02-29 09:36:44 +0100

    Adding unexpected label rule for camera rules

---
+++ edge_orchestrator/edge_orchestrator/domain/models/business_rules/camera_business_rules/unexpected_label_rule.py
@@ -0,0 +1,18 @@
+from typing import Dict, Union
+
+from edge_orchestrator.domain.models.business_rules.camera_rule import CameraRule
+from edge_orchestrator.domain.models.decision import Decision
+
+
+class UnexpectedLabelRule(CameraRule):
+    def __init__(self, unexpected_label: str):
+        self.unexpected_label = unexpected_label
+
+    def get_camera_decision(self, inference: Dict[str, Union[str, Dict]]) -> Decision:
+        camera_decision = Decision.OK
+        for inf in inference:
+            if inf in self.unexpected_label:
+                camera_decision = Decision.KO
+        print("CAMERA DESCISION", camera_decision)
+        return camera_decision
+        
