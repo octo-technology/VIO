d9b41bed63d2cb191aac3e597344d2277085fe50
Thibaut <thibaut.leibel@octo.com>
2024-03-13 09:57:09 +0100

    Removing opencv dependency

--- edge_model_serving/tflite_serving/src/tflite_serving/api_routes.py
+++ edge_model_serving/tflite_serving/src/tflite_serving/api_routes.py
@@ -3,7 +3,6 @@ from typing import Union, Any, List, Dict, AnyStr
 
 import numpy as np
 from fastapi import APIRouter, HTTPException, Request
-import cv2
 
 JSONObject = Dict[AnyStr, Any]
 JSONArray = List[Any]
@@ -46,7 +45,6 @@ async def predict(
         input_array = np.array(input_data, dtype=input_dtype)
 
         interpreter.set_tensor(input_details[0]["index"], input_array)
-
         interpreter.invoke()
         # Process image and get predictions
         prediction = {}
@@ -70,14 +68,12 @@ async def predict(
                     "detection_scores": scores.tolist(),
                 }
             }
-        elif model_name == "rocket_burn_detection":
+        elif (model_name == "rocket_burn_detection" or model_name == "plate_burn_v2_detection" or model_name ==
+              "plate_burn_v3_detection"):
+
             input_data = payload[b"inputs"]
             input_array = np.array(input_data, dtype=input_dtype)
             input_array /= 255
-
-            #
-            # TODO : Add the preprocess of the image
-            #
 
             interpreter.set_tensor(input_details[0]["index"], input_array)
             interpreter.invoke()
@@ -107,25 +103,15 @@ async def predict(
                 scores.append(float(max_score)) 
                 class_ids.append(float(max_class_id))
 
-                # TODO to eliminate close boxes 
-                # result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
-            # boxes_array = np.array(boxes)
-            # boxes_confidence_array = np.hstack((boxes, scores[:, None]))
-            # print(np.shape(boxes_confidence_array))
-            #sorted_indices = np.argsort(x[:, 4])[::-1]
+            boxes, scores, class_ids = nms(boxes, scores, class_ids)
+            severities = compute_severities(input_array[0], boxes)
 
-            i = cv2.dnn.NMSBoxes(boxes, scores, score_threshold=0.4, nms_threshold=0.45)
-            print("--- TRUC NMS ---")
-            print(i)
-            boxes = [boxes[accepted_index] for accepted_index in i]
-            class_ids = [class_ids[accepted_index] for accepted_index in i]
-            scores = [scores[accepted_index] for accepted_index in i]
-            print(boxes)
             prediction = {
                 "outputs": {
                     "detection_boxes": [boxes],
                     "detection_classes": [class_ids],
-                    "detection_scores": [scores]
+                    "detection_scores": [scores],
+                    "severities": [severities]
                 }
             }
 
@@ -142,3 +128,71 @@ async def predict(
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
+
+
+def compute_severities(image: np.array, boxes: List):
+    severities = []
+    for box in boxes:
+        severities.append(compute_box_severity(image, box))
+    return severities
+
+
+def nms(boxes, scores, class_ids, score_threshold=0.4, iou_threshold=0.45):
+    non_max_suppression_parameters_checks(score_threshold, iou_threshold)
+
+    nms_result_boxes = []
+    nms_result_scores = []
+    nms_result_classes = []
+
+    # Cut values with low confidence
+    scores = np.array(scores)
+    mask_low_confidence = scores > score_threshold
+    scores = scores[mask_low_confidence].tolist()
+    boxes = np.array(boxes)[mask_low_confidence].tolist()
+    class_ids = np.array(class_ids)[mask_low_confidence].tolist()
+
+    # Performing the Non-max suppression loop
+    while len(boxes) != 0:
+        delete_index_list = []
+
+        # Locating & Saving max confidence box
+        highest_confidence_index = scores.index(max(scores))
+        highest_confidence_box = boxes[highest_confidence_index]
+
+        nms_result_boxes.append(highest_confidence_box)
+        nms_result_scores.append(scores[highest_confidence_index])
+        nms_result_classes.append(class_ids[highest_confidence_index])
+        delete_index_list.append(highest_confidence_index)
+
+        # Iterating to analyse the iou scores
+        for index_box, box in enumerate(boxes):
+            iou = compute_iou(highest_confidence_box, box)
+            if iou > iou_threshold:
+                delete_index_list.append(index_box)
+
+        # Rebuild the box list to remove the boxes that were close to the last max score
+        boxes = [box for index_box, box in enumerate(boxes) if index_box not in delete_index_list]
+        scores = [score for index_score, score in enumerate(scores) if index_score not in delete_index_list]
+        class_ids = [class_id for index_class, class_id in enumerate(class_ids) if index_class not in delete_index_list]
+
+    return nms_result_boxes, nms_result_scores, nms_result_classes
+
+
+def non_max_suppression_parameters_checks(conf_thres, iou_thres):
+    assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
+    assert 0 <= iou_thres <= 1, f'Invalid IoU {iou_thres}, valid values are between 0.0 and 1.0'
+
+
+def compute_iou(box1, box2):
+    box1_width = box1[2] - box1[0]
+    box1_height = box1[3] - box1[1]
+    box2_width = box2[2] - box2[0]
+    box2_height = box2[3] - box2[1]
+
+    # Check if centers of boxes are close enough to be intersected
+    if ((abs((box1[0] + box1_width/2) - (box2[0] + box2_width / 2)) < 0.5 * (box2_width + box1_width)) &
+            (abs((box1[1] + box1_height/2) - (box2[1] + box2_height / 2)) < 0.5 * (box2_height + box1_height))):
+        intersection_area = (max(box1[2], box2[2]) - min(box1[0], box2[0])) * (max(box1[3], box2[3]) - min(box1[1], box2[1]))
+        union_area = box1_width * box1_height + box2_width * box2_height - intersection_area
+        return intersection_area / union_area
+    return 0
