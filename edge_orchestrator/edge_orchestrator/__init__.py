97e4f5712aeeb16ea128da86fb5601596c79b39d
Théo Couard <theo.couard@octo.com>
2024-04-29 15:05:06 +0200

    [test] changed a bit docker-compose (added powermetrics, still not able to find cpu though)

--- edge_orchestrator/edge_orchestrator/__init__.py
+++ edge_orchestrator/edge_orchestrator/__init__.py
@@ -13,3 +13,9 @@ fileConfig(
 )
 
 logger = logging.getLogger("edge_orchestrator")
+
+# TODO: define tracker
+# ROOT_PATH = Path(__file__).parents[3]
+# emissions_path = ROOT_PATH / "emissions"
+# tracker = EmissionsTracker(project_name="detection_inference", measure_power_secs=1,
+#                            tracking_mode="process", log_level="info", output_dir=str(emissions_path))
