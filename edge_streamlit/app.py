8a007125ac774430ef3b5f2a1602ff6b6e1a7be9
Andre Arockiasamy Amalor Raj <andre.amalor@octo.com>
2024-12-24 11:40:53 +0100

    Add: two cams with trigger

--- edge_streamlit/app.py
+++ edge_streamlit/app.py
@@ -1,8 +1,8 @@
+from utils import capture_videos, display_camera_checkboxes, list_cameras
 import streamlit as st
-from typing import List
-import cv2
 import requests
 import json
+import cv2
 
 # Page configuration
 st.set_page_config(
@@ -23,16 +23,21 @@ def main():
         st.session_state.recording = False
     if "selected_cameras" not in st.session_state:
         st.session_state.selected_cameras = []
+    if "trigger" not in st.session_state:
+        st.session_state.trigger = False
+    if "image" not in st.session_state:
+        st.session_state.image = None
+        
     col_1, col_2 = st.columns(2)
     if col_1.button("Active"):
         body = {
-  "config_name": "marker_classification_with_1_fake_camera"
-}
+            "config_name": "marker_classification_with_1_fake_camera"
+        }
         requests.post(url=url_config, json=body)
 
-    active_config = None
-    active_config = json.loads(requests.get(url_config).text).get("name")
-    col_2.write(f"active config: {active_config}" )
+        active_config = None
+        active_config = json.loads(requests.get(url_config).text).get("name")
+        col_2.write(f"active config: {active_config}" )
 
     # Sidebar parameters
     st.sidebar.title("Configuration")
@@ -52,55 +57,36 @@ def main():
 
     # Video capture logic
     if st.session_state.recording and selected_cameras:
-        capture_videos(selected_cameras)
+        caps = {index: cv2.VideoCapture(index) for index in selected_cameras}
+        columns = st.columns(len(selected_cameras))
+        frames_video = {index: columns[i].empty() for i, index in enumerate(selected_cameras)}
+        frames_image = {index: columns[i].empty() for i, index in enumerate(selected_cameras)}
 
+        if st.button("Trigger"):
+            payload = None
+            st.session_state.trigger = True
+            #requests.post(url=URL_ORCH + "trigger", data=payload)
+    
+       
 
-def list_cameras() -> List[str]:
-    """
-    Liste toutes les caméras disponibles sur le système.
-    """
-    index = 0
-    available_cameras = []
-    while True:
-        cap = cv2.VideoCapture(index)
-        if not cap.read()[0]:
-            break
-        available_cameras.append(f"Cam {index}")
-        cap.release()
-        index += 1
-    return available_cameras
+        while st.session_state.recording:
+            for index in selected_cameras:
+                ret, frame = caps[index].read()
+                if st.session_state.trigger:
+                    st.session_state.image = frame
+                    
+                if ret:
+                    frames_video[index].image(frame, channels="BGR")
+                else:
+                    frames_video[index].text(f"Camera {index} - Failed to capture video")
 
+                if st.session_state.image is not None and st.session_state.trigger:
+                    columns[index].image(st.session_state.image , channels="BGR")
+            st.session_state.trigger = False
 
-def display_camera_checkboxes(available_cameras: List[str]) -> List[int]:
-    """
-    Affiche les caméras disponibles sous forme de cases à cocher.
-    Retourne les indices des caméras sélectionnées.
-    """
-    selected_cameras = []
-    for i, camera_name in enumerate(available_cameras):
-        if st.sidebar.checkbox(camera_name, key=f"camera_{i}"):
-            selected_cameras.append(i)
-    return selected_cameras
+        for cap in caps.values():
+            cap.release()
 
-
-def capture_videos(selected_cameras: List[int]):
-    """
-    Capture et affiche les flux vidéo des caméras sélectionnées.
-    """
-    caps = {index: cv2.VideoCapture(index) for index in selected_cameras}
-    columns = st.columns(len(selected_cameras))
-    frames = {index: columns[i].empty() for i, index in enumerate(selected_cameras)}
-
-    while st.session_state.recording:
-        for index in selected_cameras:
-            ret, frame = caps[index].read()
-            if ret:
-                frames[index].image(frame, channels="BGR")
-            else:
-                frames[index].text(f"Camera {index} - Failed to capture video")
-
-    for cap in caps.values():
-        cap.release()
 
 
 
