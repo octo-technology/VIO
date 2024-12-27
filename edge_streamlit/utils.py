658a9fb2ad60ea1738bcd00a38167bbc9b76d560
gireg.roussel <gireg.roussel@octo.com>
2024-12-27 17:02:23 +0100

    update docker compose and add logger

--- edge_streamlit/utils.py
+++ edge_streamlit/utils.py
@@ -1,11 +1,19 @@
+import logging
 from typing import List
 import cv2
 import streamlit as st
+
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
 
 def list_cameras() -> List[str]:
     """
     Liste toutes les caméras disponibles sur le système.
     """
+    logger.info("Listing available cameras")
     index = 0
     available_cameras = []
     while True:
@@ -16,6 +24,7 @@ def list_cameras() -> List[str]:
             break
         cap.release()
         index += 1
+    logger.debug(f"{len(available_cameras)} cameras found")
     return available_cameras
 
 
@@ -25,7 +34,11 @@ def display_camera_checkboxes(available_cameras: List[str]) -> List[int]:
     Retourne les indices des caméras sélectionnées.
     """
     selected_cameras = []
-    for i, camera_name in enumerate(available_cameras):
-        if st.sidebar.checkbox(camera_name, key=f"camera_{i}"):
-            selected_cameras.append(i)
+    if len(available_cameras) == 0:
+        st.warning("No camera detected.")
+    else:
+        for i, camera_name in enumerate(available_cameras):
+            if st.sidebar.checkbox(camera_name, key=f"camera_{i}"):
+                selected_cameras.append(i)
+        logger.debug(f"Selected cameras are {selected_cameras}")
     return selected_cameras
