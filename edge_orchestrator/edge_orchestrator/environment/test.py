dad677b1a06504a6bf8b1817f35bd32e2bed713a
gireg.roussel <gireg.roussel@octo.com>
2024-12-27 11:56:06 +0100

    make unit test works

--- edge_orchestrator/edge_orchestrator/environment/test.py
+++ edge_orchestrator/edge_orchestrator/environment/test.py
@@ -31,15 +31,13 @@ class Test(Config):
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb://edge_db:27017/")
     POSTGRES_DB_URI = os.environ.get("POSTGRES_DB_URI", "postgresql://vio:vio@hub_monitoring_db:5432/vio")
     SERVING_MODEL_URL = os.environ.get("SERVING_MODEL_URL", "http://edge_model_serving:8501")
-    os.environ["ACTIVE_CONFIG_NAME"] = "station_config_TEST"
+    ACTIVE_CONFIG_NAME = "station_config_TEST"
 
     def __init__(self):
         self.metadata_storage = MongoDbMetadataStorage(self.MONGO_DB_URI)
         self.inventory = JsonInventory(TEST_INVENTORY_PATH)
         self.station_config = JsonStationConfig(TEST_STATION_CONFIGS_FOLDER_PATH, self.inventory, TEST_DATA_FOLDER_PATH)
-        self.binary_storage = FileSystemBinaryStorage(
-            TEST_DATA_FOLDER_PATH / "storage", self.station_config.active_config_name
-        )
+        self.binary_storage = FileSystemBinaryStorage(TEST_DATA_FOLDER_PATH / "storage", self.ACTIVE_CONFIG_NAME)
         self.edge_station = EdgeStation(self.station_config)
         self.model_forward = TFServingWrapper(self.SERVING_MODEL_URL, self.inventory, self.station_config)
         self.telemetry_sink = PostgresTelemetrySink(self.POSTGRES_DB_URI)
