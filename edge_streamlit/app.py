a5cf138cc058faaf014525110a090079edd5a14c
gireg.roussel <gireg.roussel@octo.com>
2024-12-27 17:03:17 +0100

    add linter

--- edge_streamlit/app.py
+++ edge_streamlit/app.py
@@ -1,18 +1,17 @@
-from utils import display_camera_checkboxes, list_cameras
-import streamlit as st
-import requests
 import json
+
 import cv2
+import requests
+import streamlit as st
+
+from utils import display_camera_checkboxes, list_cameras
 
 # Page configuration
-st.set_page_config(
-    page_title="Mon Application Streamlit",
-    page_icon="🔦",
-    layout="wide"
-)
+st.set_page_config(page_title="Mon Application Streamlit", page_icon="🔦", layout="wide")
 
 URL_ORCH = "http://localhost:8000/api/v1/"
 url_config = URL_ORCH + "configs/active"
+
 
 def main():
     """
@@ -27,17 +26,15 @@ def main():
         st.session_state.trigger = False
     if "image" not in st.session_state:
         st.session_state.image = None
-        
+
     col_1, col_2 = st.columns(2)
     if col_1.button("Active"):
-        body = {
-            "config_name": "marker_classification_with_1_fake_camera"
-        }
+        body = {"config_name": "marker_classification_with_1_fake_camera"}
         requests.post(url=url_config, json=body)
 
         active_config = None
         active_config = json.loads(requests.get(url_config).text).get("name")
-        col_2.write(f"active config: {active_config}" )
+        col_2.write(f"active config: {active_config}")
 
     # Sidebar parameters
     st.sidebar.title("Configuration")
@@ -51,42 +48,44 @@ def main():
     # Page content
     st.title("VIO Edge")
     st.markdown("Recording cameras 🌐")
-    st.write("""
+    st.write(
+        """
     This application is designed to demonstrate VIO Edge system with multiple camera feeds.
-    """)
+    """
+    )
 
     # Video capture logic
     if st.session_state.recording and selected_cameras:
         caps = {index: cv2.VideoCapture(index) for index in selected_cameras}
         columns = st.columns(len(selected_cameras))
-        frames_video = {index: columns[i].empty() for i, index in enumerate(selected_cameras)}
+        frames_video = {
+            index: columns[i].empty() for i, index in enumerate(selected_cameras)
+        }
 
         if st.button("Trigger"):
             payload = None
             st.session_state.trigger = True
-            #requests.post(url=URL_ORCH + "trigger", data=payload)
-    
-       
+            # requests.post(url=URL_ORCH + "trigger", data=payload)
 
         while st.session_state.recording:
             for index in selected_cameras:
                 ret, frame = caps[index].read()
                 if st.session_state.trigger:
                     st.session_state.image = frame
-                    
+
                 if ret:
                     frames_video[index].image(frame, channels="BGR")
                 else:
-                    frames_video[index].text(f"Camera {index} - Failed to capture video")
+                    frames_video[index].text(
+                        f"Camera {index} - Failed to capture video"
+                    )
 
                 if st.session_state.image is not None and st.session_state.trigger:
-                    columns[index].image(st.session_state.image , channels="BGR")
+                    columns[index].image(st.session_state.image, channels="BGR")
             st.session_state.trigger = False
 
         for cap in caps.values():
             cap.release()
-
-
 
 
 # Exécution du script principal
