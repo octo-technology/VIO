46545ca04e36c3b0d640c221f8213e20673cacd2
gireg.roussel <gireg.roussel@octo.com>
2025-01-23 11:06:30 +0100

    remove html objects

--- hub_streamlit/src/streamlit_component/html_objects.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def vio_hub_title_object():
-    st.sidebar.markdown(
-        "<h1 style='font-size: 2em; text-align: left;'><b>VIO Hub</b></h1>",
-        unsafe_allow_html=True,
-    )
