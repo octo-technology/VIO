9080fff54caa513690d2ee5c95fd03952e3b2181
gireg.roussel <gireg.roussel@octo.com>
2025-02-04 12:26:03 +0100

    lint

--- hub_streamlit/src/app.py
+++ hub_streamlit/src/app.py
@@ -21,13 +21,13 @@ def main():
         st.session_state.edge_names = st.session_state.gcp_client.get_edges_names()
 
     for edge_name in st.session_state.edge_names:
-        if edge_name not in st.session_state.edge_data_manager.get_edge_names():
+        if edge_name not in st.session_state.edge_data_manager.get_edges_data_names():
             edge_data = EdgeData(name=edge_name)
             edge_data.get_ip(gcp_client=st.session_state.gcp_client)
             edge_data.extract(gcp_client=st.session_state.gcp_client)
-            st.session_state.edge_data_manager.add_edge(edge_data)
+            st.session_state.edge_data_manager.add_edge_data(edge_data)
 
-        edge_data = st.session_state.edge_data_manager.get_edge(edge_name)
+        edge_data = st.session_state.edge_data_manager.get_edge_data(edge_name)
         edge_section = EdgeSection(edge_name, edge_data)
 
         if len(edge_section.use_cases_names) > 0:
--- hub_streamlit/src/models/edge_data_manager.py
+++ hub_streamlit/src/models/edge_data_manager.py
@@ -6,16 +6,16 @@ from models.edge_data import EdgeData
 
 
 class EdgeDataManager(BaseModel):
-    edges: List[EdgeData] = []
+    edges_data: List[EdgeData] = []
 
-    def add_edge(self, edge_data: EdgeData):
-        self.edges.append(edge_data)
+    def add_edge_data(self, edge_data: EdgeData):
+        self.edges_data.append(edge_data)
 
-    def get_edge_names(self) -> List[str]:
-        return [edge.name for edge in self.edges]
+    def get_edges_data_names(self) -> List[str]:
+        return [edge.name for edge in self.edges_data]
 
-    def get_edge(self, name: str) -> Optional[EdgeData]:
-        for edge in self.edges:
+    def get_edge_data(self, name: str) -> Optional[EdgeData]:
+        for edge in self.edges_data:
             if edge.name == name:
                 return edge
         return None
