f780212d5f7eac2db014cc9449a330f275b36a4d
Baptiste O'Jeanson <baptiste.ojeanson@octo.com>
2025-02-12 17:42:05 +0100

    AllItems view

--- edge_interface/src/router/index.js
+++ edge_interface/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import ItemList from '@/components/ItemList.vue';
+import AllItems from '@/components/AllItems.vue';
 import ItemDetail from '@/components/ItemDetail.vue';
 import TriggerView from '@/components/TriggerView.vue';
 import ConfigView from '@/components/ConfigView.vue';
@@ -12,8 +12,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'ItemList',
-      component: ItemList
+      name: 'AllItems',
+      component: AllItems
     },
     {
       path: '/item/:id',
