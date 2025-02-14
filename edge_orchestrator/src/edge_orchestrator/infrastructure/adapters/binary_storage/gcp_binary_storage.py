95803c5cfcea99abdd37fe4c0dd9b194ea528f68
Baptiste O'Jeanson <baptiste.ojeanson@octo.com>
2025-02-14 08:35:32 +0100

    gcp binary storage

--- edge_orchestrator/src/edge_orchestrator/infrastructure/adapters/binary_storage/gcp_binary_storage.py
+++ edge_orchestrator/src/edge_orchestrator/infrastructure/adapters/binary_storage/gcp_binary_storage.py
@@ -1,16 +1,60 @@
+from http.client import HTTPException
 import logging
+from pathlib import Path
+from typing import Dict, List
+from uuid import UUID
 
 from edge_orchestrator.domain.models.item import Item
 from edge_orchestrator.domain.models.storage.storage_config import StorageConfig
 from edge_orchestrator.domain.ports.binary_storage.i_binary_storage import (
     IBinaryStorage,
 )
+from google.cloud import storage
 
 
 class GCPBinaryStorage(IBinaryStorage):
-    def __init__(self, storage_config: StorageConfig):
-        self._storage_config: StorageConfig = storage_config
+    def __init__(self, binary_storage_config: StorageConfig):
+        self._storage_config: StorageConfig = binary_storage_config
         self._logger = logging.getLogger(__name__)
+        self._storage_client = storage.Client()
+        self._bucket = self._storage_client.get_bucket(binary_storage_config.target_directory.as_posix())
 
     def save_item_binaries(self, item: Item):
-        raise NotImplementedError("Not implemented yet")
+        for camera_id, image in item.binaries.items():
+            blob = self._bucket.blob(self._get_storing_path(item.id) / f"{camera_id}.jpg")
+            if blob is None:
+                raise Exception("An image should be upload")
+            blob.upload_from_string(image.image_bytes, content_type="image/jpg")
+
+    def get_item_binary_names(self, item_id: UUID) -> List[str]:
+        binaries = []
+        for blob in self._bucket.list_blobs(self._get_storing_path(item_id)):
+            if item_id in blob.name:
+                binaries.append(blob.name)
+        return binaries
+
+    def get_item_binaries(self, item_id: UUID) -> Dict[str, bytes]:
+        binaries = {}
+        for blob in self._bucket.list_blobs(prefix=self._get_storing_path(item_id).as_posix()):
+            if item_id in blob.name:
+                binary = blob.download_as_bytes()
+                camera_id = Path(blob.name).stem
+                binaries[camera_id] = binary
+        return binaries
+
+    def get_item_binary(self, item_id: UUID, camera_id: str) -> bytes:
+        filename = self._get_storing_path(item_id) / f"{camera_id}.jpg"
+        blob = self._bucket.get_blob(filename)
+        if blob is None:
+            raise HTTPException(status_code=400, detail=f"The item {item_id} has no binary for {camera_id}")
+        return blob.download_as_bytes()
+
+    def _get_storing_directory_path(self) -> Path:
+        return (
+            self._storage_config.target_directory / self._storage_config.prefix
+            if self._storage_config.prefix
+            else self._storage_config.target_directory
+        )
+
+    def _get_storing_path(self, item_id: UUID) -> Path:
+        return self._get_storing_directory_path() / str(item_id)
