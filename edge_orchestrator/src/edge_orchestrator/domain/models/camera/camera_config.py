5a0e7d8338d3b4fa7f2467a83c762f7038755120
Baptiste O'Jeanson <baptiste.ojeanson@octo.com>
2025-02-14 20:27:19 +0100

    fix webcam capture

--- edge_orchestrator/src/edge_orchestrator/domain/models/camera/camera_config.py
+++ edge_orchestrator/src/edge_orchestrator/domain/models/camera/camera_config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, model_validator
 
 from edge_orchestrator.domain.models.camera.camera_type import CameraType
 from edge_orchestrator.domain.models.camera_rule.camera_rule_config import (
@@ -24,3 +24,9 @@ class CameraConfig(BaseModel):
     model_forwarder_config: Optional[ModelForwarderConfig] = None
     camera_rule_config: Optional[CameraRuleConfig] = None
     recreate_me: Optional[bool] = False
+
+    @model_validator(mode="after")
+    def check_params(self):
+        if self.camera_type in [CameraType.FAKE, CameraType.USB, CameraType.WEBCAM] and self.source_directory is None:
+            raise ValueError("source_directory is required with camera_type FAKE, USB or WEBCAM")
+        return self
--- edge_orchestrator/src/edge_orchestrator/infrastructure/adapters/camera/webcam_camera.py
+++ edge_orchestrator/src/edge_orchestrator/infrastructure/adapters/camera/webcam_camera.py
@@ -1,9 +1,9 @@
 import logging
-from typing import Iterator
+from threading import Thread
 
 import cv2
+from edge_orchestrator.domain.models.camera.camera_type import CameraType
 import numpy as np
-from tenacity import retry, stop_after_attempt, wait_fixed
 
 from edge_orchestrator.domain.models.binary import Image
 from edge_orchestrator.domain.models.camera.camera_config import CameraConfig
@@ -14,34 +14,38 @@ class WebcamCamera(ICamera):
     def __init__(self, camera_config: CameraConfig):
         self._camera_config = camera_config
         self._logger = logging.getLogger(__name__)
-        self._cap = None
-        self._open_webcam(iter([0, 1, 2, 3]))
+        self._open_webcam(self._camera_config.source_directory.as_posix())
+        self._start()
 
-    @retry(stop=stop_after_attempt(4), wait=wait_fixed(2))
-    def _open_webcam(self, device_stream: Iterator):
-        self._cap = cv2.VideoCapture(next(device_stream))
-
-        if not self._cap.isOpened():
-            raise cv2.error("Could not open webcam")
-
+    def _open_webcam(self, src: str):
+        self._stream = cv2.VideoCapture(src)
+        (self._grabbed, self._frame) = self._stream.read()
+        self._name = __name__
+        self._stopped = False
         if self._camera_config.camera_resolution:
-            self._cap.set(cv2.CAP_PROP_FRAME_WIDTH, self._camera_config.camera_resolution.width)
-            self._cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self._camera_config.camera_resolution.height)
+            self._stream.set(cv2.CAP_PROP_FRAME_WIDTH, self._camera_config.camera_resolution.width)
+            self._stream.set(cv2.CAP_PROP_FRAME_HEIGHT, self._camera_config.camera_resolution.height)
+
+    def _start(self):
+        t = Thread(target=self._update, name=self._name, args=())
+        t.daemon = True
+        t.start()
+
+    def _update(self):
+        while True:
+            if self._stopped:
+                return
+            (self._grabbed, self._frame) = self._stream.read()
 
     def _release_webcam(self):
-        try:
-            self._cap.release()
-        except cv2.error:
-            self._logger.warning("Could not release properly the webcam")
+        self._stopped = True
+        self._stream.release()
 
-    def capture(self, attempt=0) -> Image:
-        exit_code, frame = self._cap.read()
-        if not exit_code and attempt < 5:
-            self._logger.error("Failed to capture image")
-            self._open_webcam(iter([0, 1, 2, 3]))
-            self.capture(attempt + 1)
-
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        exit_code, img_encode = cv2.imencode(".jpg", frame_rgb)
+    def capture(self) -> Image:
+        if not self._grabbed and self._frame is None:
+            self._logger.error("Error while capturing image")
+            return Image()
+        frame = self._frame
+        _, img_encode = cv2.imencode(".jpg", frame)
         img_as_array = np.array(img_encode)
         return Image(image_bytes=img_as_array.tobytes())
