226bfbb40f619afe35ed33b7dd76de908da3c12f
gireg.roussel <gireg.roussel@octo.com>
2025-03-07 15:01:25 +0100

    update cleaner

--- edge_streamlit/components/image_acquisition.py
+++ edge_streamlit/components/image_acquisition.py
@@ -8,32 +8,61 @@ import streamlit as st
 from matplotlib import pyplot as plt
 
 from streamlit_webrtc import webrtc_streamer
+import av
+import time
 
 
 def image_acquisition():
+    
     lock = threading.Lock()
-    img_container = {"img": None}
-
+    images = []
+    last_timestamp = {"seconds": None}
 
     def video_frame_callback(frame):
+        number_seconds = 5
         img = frame.to_ndarray(format="bgr24")
         with lock:
-            img_container["img"] = img
+            # print("img_container", img_container)
+            # print("last_timestamp", last_seconds["seconds"])
+            image_obj = {}
+
+            if last_timestamp["seconds"] is None:
+                last_timestamp["seconds"] = time.time()
+            # print("last_timestamp", last_seconds["seconds"])
+
+            current_time = time.time()
+            # print("current_time", current_time)
+            # print("current_time - last_timestamp", current_time - last_seconds["seconds"] >= number_seconds)
+            if len(images) == 0 or current_time - last_timestamp["seconds"] >= number_seconds:
+                print(f"Acquisition every {number_seconds} seconds")
+                last_timestamp["seconds"] = current_time
+                image_obj["img"] = img
+            
+                images.append(image_obj)
 
         return frame
 
 
     ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
 
+
     fig_place = st.empty()
     fig, ax = plt.subplots(1, 1)
 
     while ctx.state.playing:
         with lock:
-            img = img_container["img"]
-        if img is None:
+            image = images[-1]["img"] if len(images) > 0 else None
+
+        if image is None:
             continue
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ax.cla()
-        ax.hist(gray.ravel(), 256, [0, 256])
-        fig_place.pyplot(fig)
+        print("Image acquisition", len(images))
+        if len(images) > 0:
+            ax.clear()
+            ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            ax.axis('off')
+            fig_place.pyplot(fig)
+
+    # if len(images) > 0:
+    #     st.write("Image acquisition")
+    #     for img_obj in images:
+    #         st.image(img_obj["img"], channels="BGR")
