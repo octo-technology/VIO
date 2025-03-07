65bda2b423b280c8a18d18df6362a54d296e73f2
Baptiste O'Jeanson <baptiste.ojeanson@octo.com>
2025-03-07 18:17:31 +0100

    refactor how usb camera are handled (auto detection)

--- edge_orchestrator/src/edge_orchestrator/domain/models/camera/camera_config.py
+++ edge_orchestrator/src/edge_orchestrator/domain/models/camera/camera_config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Optional
 
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, computed_field, model_validator
 
 from edge_orchestrator.domain.models.camera.camera_type import CameraType
 from edge_orchestrator.domain.models.camera_rule.camera_rule_config import (
@@ -13,12 +13,15 @@ from edge_orchestrator.domain.models.model_forwarder.image_resolution import (
 from edge_orchestrator.domain.models.model_forwarder.model_forwarder_config import (
     ModelForwarderConfig,
 )
+from edge_orchestrator.utils.usb_device import get_camera_device_node
 
 
 class CameraConfig(BaseModel):
     camera_id: str
     camera_type: CameraType
     source_directory: Optional[Path] = None
+    camera_vendor: Optional[str] = None
+    camera_serial_number: Optional[str] = None
     position: Optional[str] = "front"
     camera_resolution: Optional[ImageResolution] = None
     model_forwarder_config: Optional[ModelForwarderConfig] = None
@@ -26,6 +29,19 @@ class CameraConfig(BaseModel):
 
     @model_validator(mode="after")
     def check_params(self):
-        if self.camera_type in [CameraType.FAKE, CameraType.USB, CameraType.WEBCAM] and self.source_directory is None:
-            raise ValueError("source_directory is required with camera_type FAKE, USB or WEBCAM")
+        if self.camera_type == CameraType.FAKE and self.source_directory is None:
+            raise ValueError("source_directory is required with camera_type FAKE")
+        if (
+            self.camera_type in [CameraType.USB, CameraType.WEBCAM]
+            and self.camera_vendor is not None
+            and self.camera_serial_number is None
+        ):
+            raise ValueError("camera_vendor and camera_serial_number are required with camera_type USB or WEBCAM")
         return self
+
+    @computed_field
+    @property
+    def device_node(self) -> Optional[str]:
+        if self.camera_type in [CameraType.USB, CameraType.WEBCAM]:
+            return get_camera_device_node(self.camera_vendor, self.camera_serial_number)
+        return None
