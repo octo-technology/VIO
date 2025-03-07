383c28367d7505e76dc39b18af0c5958249cfcee
gireg.roussel <gireg.roussel@octo.com>
2025-03-07 09:35:02 +0100

    [feat] test on streamlit_webrtc package

--- edge_streamlit/components/image_acquisition.py
+++ edge_streamlit/components/image_acquisition.py
@@ -1,10 +1,39 @@
 
 import streamlit as st
 
+import threading
+
+import cv2
+import streamlit as st
+from matplotlib import pyplot as plt
+
+from streamlit_webrtc import webrtc_streamer
+
 
 def image_acquisition():
-    picture = st.camera_input("Take a picture")
+    lock = threading.Lock()
+    img_container = {"img": None}
 
-    if picture:
-        st.image(picture)
-        st.write("Picture taken!")
+
+    def video_frame_callback(frame):
+        img = frame.to_ndarray(format="bgr24")
+        with lock:
+            img_container["img"] = img
+
+        return frame
+
+
+    ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+
+    fig_place = st.empty()
+    fig, ax = plt.subplots(1, 1)
+
+    while ctx.state.playing:
+        with lock:
+            img = img_container["img"]
+        if img is None:
+            continue
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ax.cla()
+        ax.hist(gray.ravel(), 256, [0, 256])
+        fig_place.pyplot(fig)
